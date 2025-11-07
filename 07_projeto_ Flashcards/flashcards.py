78ccf9d44a0e035464a3fb6335d67a79a056cc82
oliviawyy <olivia.c@aluno.senai.br>
2025-11-07 10:56:33 -0300

    quase acabando

--- 07_projeto_ Flashcards/flashcards.py
+++ 07_projeto_ Flashcards/flashcards.py
@@ -6,9 +6,10 @@ import sqlite3
 
 
 class FlashcardC():
-    def __init__(self):
+    def __init__(self, JanelaPai):
+        self.janelapai = JanelaPai
         # criando a janela
-        self.janela = tk.Tk()
+        self.janela = tk.Toplevel(JanelaPai)
         self.janela.title("Flashcards")
         self.janela.geometry("800x700")
         self.janela.resizable(False, False)
@@ -44,10 +45,10 @@ class FlashcardC():
                                    foreground="lightskyblue")
         pergunta_label.pack(pady=(10, 0))
 # relief="solid",
-        self.pergunta_text = tk.Text(self.janela, 
-                                     height=2, 
+        self.pergunta_text = tk.Entry(self.janela, 
+                                      
                                      width=50, 
-                                     wrap="word", 
+                                      
                                      borderwidth=1, 
                                      foreground="blue")
         self.pergunta_text.pack(pady=(0, 10))
@@ -59,10 +60,10 @@ class FlashcardC():
                                    foreground="lightskyblue")
         resposta_label.pack(pady=(10, 0))
 
-        self.resposta_text = tk.Text(self.janela, 
-                                     height=2, 
+        self.resposta_text = tk.Entry(self.janela, 
+                                      
                                      width=50, 
-                                     wrap="word",
+                                     
                                      borderwidth=1, 
                                      foreground="blue",
                                      )
@@ -70,8 +71,8 @@ class FlashcardC():
 
         # Botão para salvar
         salvar_button = ttk.Button(self.janela, 
-                                   text="Salvar Cartão", 
-                                   command=self.salvar_cartao)
+                                   text="Adicionar Cartao", 
+                                   command=self.inserir_disciplina)
         salvar_button.pack(pady=10)
 
         frame_botao = ttk.Frame(self.janela)
@@ -84,74 +85,164 @@ class FlashcardC():
                                    width=20) 
         botao_excluir.pack(side="left",padx=10)
 
+        # botao para ir para o modo estudo
+        botao_estudo = ttk.Button(self.janela,
+                                  text="Modo Estudo",
+                                  command=self.modo_estudo
+                                  )
+
+    def modo_estudo(self):
+        
+             
 
         # Criando o Treeview (apenas uma vez)
-        self.treeview = ttk.Treeview(self.janela, columns=("disciplina", "pergunta", "resposta"), show="headings")
+        self.treeview = ttk.Treeview(self.janela, columns=("codigo", "disciplina", "pergunta", "resposta"), show="headings")
         self.treeview.pack(pady=20)
 
         # Configurando as colunas do Treeview
+        self.treeview.heading("codigo", text="Codigo")
         self.treeview.heading("disciplina", text="Disciplina")
         self.treeview.heading("pergunta", text="Pergunta")
         self.treeview.heading("resposta", text="Resposta")
 
+        self.treeview.column("codigo", anchor="center", width=150)
         self.treeview.column("disciplina", anchor="center", width=150)
-        self.treeview.column("pergunta", anchor="center", width=300)
-        self.treeview.column("resposta", anchor="center", width=300)
+        self.treeview.column("pergunta", anchor="center", width=200)
+        self.treeview.column("resposta", anchor="center", width=200)
+
+    # criando banco de dados
+        conexao = sqlite3.connect("./bd_projeto_flashcards.sqlite3")
+
+        cursor = conexao.cursor()
+
+       
+
+        cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS disciplina(
+                        codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        disciplina VARCHAR(50) ,
+                        pergunta VARCHAR(50) ,
+                        resposta VARCHAR(50)
+                       
+                       
+                       )             
+        """)
+
+        # INTEGER ARMAZENAR NUMEROS INTEIROS
+
+        # atualizando lista aqui
+        self.atualizar_tabela()
+        # inicio a conexao
+        conexao.commit()
+
+        # encerro a conexão
+        conexao.close()
+
+    def atualizar_tabela(self):
+        #  vai ver todos os itens da treeview e deletar um por um
+        for item in self.treeview.get_children():
+              self.treeview.delete(item)
+
+        conexao = sqlite3.connect("bd_projeto_flashcards.sqlite3")
+
+        cursor = conexao.cursor()
+
+        sql_atualizar = """
+                        SELECT codigo, disciplina, pergunta, resposta FROM disciplina;
+                        """
+        
+        cursor.execute(sql_atualizar)
+
+        # fetchall - para pegar todos os itens da consulta
+        disciplinas = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+
+        # add no treeview
+        for linha in disciplinas:
+             self.treeview.insert("", "end", values=linha)
+
 
     def salvar_cartao(self):
         # Obtendo os valores dos campos de texto
         disciplina = self.disciplina_entry.get().strip()
-        pergunta = self.pergunta_text.get("1.0", tk.END).strip()
-        resposta = self.resposta_text.get("1.0", tk.END).strip()
+        pergunta = self.pergunta_text.get().strip()
+        resposta = self.resposta_text.get().strip()
 
         # Adicionando os dados ao Treeview
         if disciplina and pergunta and resposta:
             self.treeview.insert("", "end", values=(disciplina, pergunta, resposta))
 
             # Limpando os campos de texto
-            self.disciplina_entry.delete(0, tk.END)
-            self.pergunta_text.delete("1.0", tk.END)
-            self.resposta_text.delete("1.0", tk.END)
+            self.disciplina_entry.delete(0, "end")
+            self.pergunta_text.delete(0, "end")
+            self.resposta_text.delete(0, "end")
 
         # excluir carta
     def excluir_cartao(self):
-        excluir_dados = self.lista.curselection()
+        excluir_dados = self.treeview.selection()
 
         if excluir_dados:
-                texto_tarefa = self.lista.get(excluir_dados[0])  # Obter o texto da tarefa
-                self.lista.delete(excluir_dados)
+                codigo_disciplina = self.treeview.item(excluir_dados)["values"][0]
+                
+                self.treeview.delete(excluir_dados)
 
-                conexao = sqlite3.connect("./bd_lista_tarefas.sqlite")
+                conexao = sqlite3.connect("bd_projeto_flashcards.sqlite3")
                 cursor = conexao.cursor()
 
                 sql_delete = """
-                                DELETE FROM tarefa WHERE descricao_tarefa = ?;
+                                DELETE FROM disciplina WHERE codigo = ?;
                             """
-                cursor.execute(sql_delete, (texto_tarefa,))  # Corrigido
+                cursor.execute(sql_delete, (codigo_disciplina,))  # Corrigido
                 conexao.commit()
 
                 cursor.close()
                 conexao.close()
         else:
-                messagebox.showerror(message="Selecione um item antes de excluir")
+                messagebox.showerror(message="Adicione um item antes de salvar")
 
-    def banco_dados(self):
+   
+    def inserir_disciplina(self):
+
+        disciplina = self.disciplina_entry.get()
+        pergunta = self.pergunta_text.get()
+        resposta = self.resposta_text.get()
+
+        # criar conexao
         conexao = sqlite3.connect("./bd_projeto_flashcards.sqlite3")
 
-        cursor = conexao.cursor()
+        # criar cursor
+        cursor =  conexao.cursor()
 
-        cursor.execute("""
-                        CREATE TABLE IF NOT EXIST disciplina(
-                        disciplina VARCHAR(50) ,
-                        pergunta VARCHAR(50) ,
-                        resposta VARCHAR(50) PRIMARY KEY
-                       
-                       )             
-        """)
+        # executar
 
+        sql_insert = f""" 
+                        INSERT INTO disciplina
+                            (disciplina,
+                            pergunta,
+                            resposta)
+                            VALUES
+                            (?,
+                            ?,
+                            ?);
+                        """
+        valores = [disciplina, pergunta, resposta]
 
+        cursor.execute(sql_insert, valores)
 
+        self.atualizar_tabela()
         
+    
+        # comitar
+        conexao.commit()
+
+            # fechar conexão
+        conexao.close()
+
+        novo_id = cursor.lastrowid
+                
+        self.treeview.insert("","end", values=[novo_id, disciplina, pergunta, resposta])
+
 
     def run(self):
         self.janela.mainloop()
--- 07_projeto_ Flashcards/modo_estudo.py
+++ 07_projeto_ Flashcards/modo_estudo.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import Tk
-
+from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 
@@ -11,19 +10,98 @@ class Estudo():
         # criando a janela
         self.janela = tk.Tk()
         self.janela.title("Modo Estudo")
-        self.janela.geometry("800x700")
+        self.janela.geometry("800x500")
         self.janela.resizable(False, False)
-
-        self.botao_estudo = tk.Button(self.janela, text="Inicar Modo Estudo")
-
 
 #Desafio Extra:
 #Implementar um 'Modo Estudo'. Numa nova janela (`Toplevel`), o sistema mostra a *pergunta* de um cartão aleatório. O usuário clica num botão 'Revelar Resposta' para ver a resposta.
         #criando um rótulo
-      
 
-    # Função para iniciar o modo de estudo
-    
+        self.rotulo = tk.Label(self.janela,
+                        text="Modo Estudo  ๋࣭ ࣪ ˖🎐",
+                        background="lightskyblue",
+                        foreground="navy",
+                        font=("Kristen ITC", 20))
+        self.rotulo.pack(pady=30)
+
+        # Campo para pergunta
+        pergunta_label = tk.Label(self.janela, 
+                                   text="Pergunta:", 
+                                   font=("Kristen ITC", 12), 
+                                   foreground="lightskyblue")
+        pergunta_label.pack(pady=(10, 0))
+# relief="solid",
+        self.pergunta_text = tk.Label(self.janela, 
+                                     width=50,
+                                     borderwidth=1, 
+                                     foreground="blue",
+                                     relief="solid")
+        self.pergunta_text.pack(pady=(20, 0))
+
+
+
+        frame_botoes = ttk.Frame(self.janela)
+        frame_botoes.pack()
+
+        botao_revelar = ttk.Button(frame_botoes,command=self.resposta ,text="Revelar Resposta")
+        botao_revelar.pack(side="right", padx=5, pady=5)
+
+        botao_aleatorio = ttk.Button(frame_botoes,command=self.alterar_pergunta ,text="Pergunta Aleatória")
+        botao_aleatorio.pack(side="left",padx=5, pady=5)
+
+        # Campo para resposta
+        resposta_label = tk.Label(self.janela, 
+                                   text="Resposta:", 
+                                   font=("Kristen ITC", 12), 
+                                   foreground="lightskyblue")
+        resposta_label.pack(pady=(20, 0))
+
+        self.respondida = ttk.Label(self.janela,
+                               width=50,
+                               borderwidth=1, 
+                               relief="solid",
+                               )
+        self.respondida.pack(pady=(20, 0))
+
+    def alterar_pergunta(self):
+        conexao = sqlite3.connect("bd_projeto_flashcards.sqlite3")
+
+        cursor = conexao.cursor()
+
+        sql_alterar = """
+                        SELECT pergunta FROM disciplina
+                        ORDER BY RANDOM()
+
+                        """
+        cursor.execute(sql_alterar)
+        self.perguntas = cursor.fetchone()
+
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+        
+
+        self.pergunta_text.configure(text=f"{self.perguntas}")
+
+    def resposta(self):
+        conexao = sqlite3.connect("bd_projeto_flashcards.sqlite3")
+
+        cursor = conexao.cursor()
+
+        sql_alterar = """
+                        SELECT resposta FROM disciplina
+                        WHERE pergunta = ?;
+
+                        """
+        valores = self.perguntas
+        cursor.execute(sql_alterar, valores)
+        self.respostas = cursor.fetchone()
+
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        self.respondida.configure(text=f"{self.respostas}")
     
     
     def run(self):
